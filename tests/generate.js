35517f886848a6f1c7ed7cc1a00f04a1afdb33e9
Yuan Yuchuan <yyc478@gmail.com>
2016-02-20 22:20:40 +0800

    Student info generation working

--- tests/generate.js
+++ tests/generate.js
@@ -8,17 +8,45 @@ var rand = Random.engines.mt19937();
 rand.seed(123);
 
 describe("Generating Test Data", function(){
-    it("Should generate a file", function(done){
-        set = new Set();
-        for(i = 0; i < 180; i++){
+    var names = [];
+    var majors = [];
+    var pMales = {};
+    it("Should produce an array of names", function(){
+        var set = new Set();
+        for(i = 0; i < numStudents; i++){
             set.add(generateRandomName());
         }
         names = Array.from(set.values());
+    });
+    it("Should list majors and genders", function(){
+        
+        var distributions = XLSX.readFile("tests/studentRatio.xlsx");
+        var worksheet = distributions.Sheets[distributions.SheetNames];
+        var mi = 0;
+        for(var i = 1; i < 39; i++){
+            var majorName = worksheet['B'+i].v;
+            var numStudents = worksheet['C' + i].v;
+            var PMale = worksheet['D'+i].v;
+            for(var j = 0; j < numStudents; j++){
+                majors[mi++] = majorName;
+            }
+            pMales[majorName] = PMale;
+        }
+        Random.shuffle(mt, majors);
+    });
+    
+    it("Should write everything to tests/test.txt", function(done){
         fs.open("tests/test.txt", "w+", function(err, file){
+            names = names.map(function(val, index){
+                var major = majors[index];
+                //Generate gender based on the major's proportion
+                var gender = (Random.bool(pMales[major]))(mt) ? "M": "F";
+                console.log(val + ";" + major + ";" + gender);
+                return val + ";" + major + ";" + gender;
+            });
             names = names.join("\n");
             fs.write(file, names, 0, 0, done);
-            
-        })
+        });
     });
 });
 
--- tests/test.js
+++ tests/test.js
@@ -1,30 +1,21 @@
 var Random = require("random-js");
 var rn = require("random-name");
+var XLSX = require("xlsx");
 var fs = require("fs")
 
 var Student = require("../Student.js");
 
-var rand = Random.engines.mt19937();
-rand.seed(123);
+var mt = Random.engines.mt19937();
+mt.seed(123);
 
-describe("Generating Test Data", function(){
-    it("Should generate a file", function(done){
-        set = new Set();
-        for(i = 0; i < 180; i++){
-            set.add(generateRandomName());
-        }
-        names = Array.from(set.values());
-        fs.open("tests/test.txt", "w+", function(err, file){
-            names = names.join("\n");
-            fs.write(file, names, 0, 0, done);
-            
-        })
-    });
-});
+var numStudents = 180;
+
+
+//describe("Check for equality", function())
 
 function generateRandomName(){
     string = rn.first();
-    while((Random.bool())(rand)){
+    while((Random.bool())(mt)){
         string += " " + rn.middle();
     }
     string += " " + rn.last();
