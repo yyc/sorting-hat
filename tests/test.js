863c96a8430dfbdc1b1d158c360fc41399fe2d80
Yuan Yuchuan <yyc478@gmail.com>
2016-02-20 20:10:15 +0800

    Add name generation

--- tests/test.js
+++ tests/test.js
@@ -1,23 +1,33 @@
 var Random = require("random-js");
+var rn = require("random-name");
 var fs = require("fs")
 
 var Student = require("../Student.js");
 
 var rand = Random.engines.mt19937();
+rand.seed(123);
 
 describe("Generating Test Data", function(){
     it("Should generate a file", function(done){
         set = new Set();
-        for(i = 0; i < 180, i++){
-            generateRando
+        for(i = 0; i < 180; i++){
+            set.add(generateRandomName());
         }
-        fs.open("test.txt", "w+", function(err, file){
-            fs.write(file, "hihi", 0, 0, done);
+        names = Array.from(set.values());
+        fs.open("tests/test.txt", "w+", function(err, file){
+            names = names.join("\n");
+            fs.write(file, names, 0, 0, done);
             
         })
     });
 });
 
 function generateRandomName(){
+    string = rn.first();
+    while((Random.bool())(rand)){
+        string += " " + rn.middle();
+    }
+    string += " " + rn.last();
+    return string;
     
 }
