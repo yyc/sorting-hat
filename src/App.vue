a353d08f3e49f8d0bbc7af210ddb1da536577b4b
Yuchuan <yyc478@gmail.com>
2019-06-14 23:47:10 +0800

    get control panel bindings correct

--- src/App.vue
+++ src/App.vue
@@ -1,27 +1,48 @@
 <template>
   <div id="app">
     <dropzone v-if="table == undefined" @receivedTable="handleTable"></dropzone>
+    <div v-if="table !== undefined">
+      <div id="previewtable" v-html="previewTable"></div>
+      <configpanel v-bind:config="config"></configpanel>
+    </div>
   </div>
 </template>
 
 <script>
 import Dropzone from "./components/Dropzone";
+import Configpanel from "./components/configpanel";
+
+import XLSX from "xlsx";
 
 function handleTable(tableData, options = {}) {
   this.table = tableData;
   console.log(this.table);
 }
 
+function previewTable() {
+  if (this.config.showSortingOnly == false) {
+    return XLSX.utils.sheet_to_html(this.table);
+  } else {
+  }
+}
+
 export default {
   name: "App",
   components: {
-    Dropzone
+    Dropzone,
+    Configpanel
   },
   data: () => ({
-    table: undefined
+    table: undefined,
+    config: {
+      showSortingOnly: false
+    }
   }),
   methods: {
     handleTable
+  },
+  computed: {
+    previewTable
   }
 };
 </script>
@@ -34,5 +55,9 @@ export default {
   text-align: center;
   color: #2c3e50;
   margin-top: 60px;
+}
+#previewtable {
+  height: 500px;
+  overflow: scroll;
 }
 </style>
---
+++ src/components/configpanel.vue
@@ -0,0 +1,38 @@
+<template>
+  <div class="configpanel">
+    Sorting Configuration:
+    <br />
+
+    <input type="checkbox" id="show-sorting" v-model="config.showSortingOnly" />
+    <label for="show-sorting"> Show Sorting Columns Only</label>
+    <br />
+    <!-- <configjson v-bind:config="config"></configjson> -->
+  </div>
+</template>
+
+<script>
+import configjson from "./configjson";
+
+let defaultConfig = {};
+
+export default {
+  name: "Configpanel",
+  components: {
+    configjson
+  },
+  props: {
+    config: {
+      type: Object,
+      required: true
+    }
+  },
+  data: function() {
+    console.log(this.config);
+    return {
+      myconfig: this.config
+    };
+  }
+};
+</script>
+
+<style></style>
